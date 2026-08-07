4edd9a6dded9bc2fe528beaf33bd34e9ea9cc23d
gvbvdxxalt2 <gvbvdxxalt2@outlook.com>
2026-08-07 12:18:47 +0000

    Deploying to deploy from @ gvbvdxxalt2/SRB2web@c3ff49a8bbe18cb6326e1e5e7555d36e506f91d5 🚀

--- launcher.bundle.js
+++ launcher.bundle.js
@@ -3567,12 +3567,10 @@ window.StartedMainLoopCallback = function () {
   // Add mousemove listener for manual mouse delta handling
   document.addEventListener("mousemove", (e) => {
     if (document.pointerLockElement === gameCanvas) {
-      Module.ccall(
-        "SRB2_AddMouseDelta",
-        "void",
-        ["number", "number"],
-        [Math.round(e.movementX), Math.round(e.movementY)],
-      );
+      Module.ccall("SRB2_AddMouseDelta", "void", ["number", "number"], [
+        Math.round(e.movementX),
+        Math.round(e.movementY),
+      ]);
     }
   });
 
@@ -3766,16 +3764,6 @@ setInterval(() => {
     }
   }
 }, 1000 / 55);
-gameCanvas.addEventListener(
-  "mousemove",
-  (e) => {
-    if (document.pointerLockElement === gameCanvas) {
-      mouseMoveX += e.movementX;
-      mouseMoveY += e.movementY;
-    }
-  },
-  true,
-);
 window.addEventListener(
   "load",
   (_) => {
