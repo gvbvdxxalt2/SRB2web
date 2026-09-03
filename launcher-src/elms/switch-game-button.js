cac3a12f576e4b4425b68ee690fe7dafee69e6dd
Jason Glenn Evans <gvbvdxxalt2@outlook.com>
2026-09-03 09:17:14 -0400

    Finally making SRB2Kart web public today

---
+++ launcher-src/elms/switch-game-button.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    element: "a",
+    className: "button switchGameButton",
+    href: "https://kartweb.gvbvdxx.me",
+    target: "_blank",
+    children: [
+      {
+        element: "img",
+        src: "images/karticon.png",
+        style: {
+          height: "32px",
+        },
+      },
+      {
+        element: "span",
+        textContent: "Switch to SRB2Kart Web",
+      },
+    ],
+  },
+];
