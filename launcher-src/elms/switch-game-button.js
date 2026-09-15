a51032a3140610468e52e921193635b625357feb
Jason Glenn Evans <gvbvdxxalt2@outlook.com>
2026-09-15 08:42:54 -0400

    Switch game buttons

--- launcher-src/elms/switch-game-button.js
+++ launcher-src/elms/switch-game-button.js
@@ -1,21 +1,30 @@
-module.exports = [
-  {
+function retSwitchGameButton(name, icon, link) {
+  return {
     element: "a",
     className: "button switchGameButton",
-    href: "https://kartweb.gvbvdxx.me",
+    href: link,
     target: "_blank",
     children: [
       {
         element: "img",
-        src: "images/karticon.png",
+        src: icon,
         style: {
           height: "32px",
         },
       },
       {
         element: "span",
-        textContent: "Switch to SRB2Kart Web",
+        textContent: `Switch to ${name}`,
       },
     ],
-  },
+  };
+}
+
+module.exports = [
+  //Comment the current port out, keep the others uncommented.
+  //Allows for quick switching between ports (nobody wants to dig through links).
+
+  //retSwitchGameButton("SRB2 Web", "images/ports/srb2web.png", "https://srb2web.gvbvdxx.me"),
+  retSwitchGameButton("SRB2Kart Web", "images/ports/kartweb.png", "https://kartweb.gvbvdxx.me"),
+  retSwitchGameButton("SRB2Legacy Web", "images/ports/legacyweb.png", "https://legacyweb.gvbvdxx.me"),
 ];
